6ecb34427a1a9903a4c52e6ab7330c54ee8a9817
Carl Sutherland <carl@humblesoftware.com>
2011-03-10 19:59:33 -0500

    Added update method.

--- js/Sliders.js
+++ js/Sliders.js
@@ -16,26 +16,26 @@ Humble( function () {
 
         render : function () {
 
-            var items   = this.model.getItems(),
-                node    = this.node;
+            var dimensions  = Humble.Config.DVZ.budget.dimensions,
+                sliders     = {},
+                node        = this.node;
 
-            _.each(items, function (item, key) {
-                var slider = this._renderSlider(item);
+            _.each(dimensions, function (name, key) {
+                var slider = this._renderSlider(name);
                 node.append(slider);
                 slider.data('key', key);
                 sliders[key] = slider;
             }, this);
 
-            this.bind();
+            this._sliders = sliders;
         },
 
-        _renderSlider : function (item) {
+        _renderSlider : function (name) {
 
             var slider = $('<div class="slider"></div>'),
                 widget = $('<div></div>'),
-                total  = this.model.getTotalTaxes(),
-                value  = item['mycosti'],
-                title  = item['dimensionName'],
+                value  = 0,
+                title  = name,
                 config;
 
             slider.append('<div class="value mycosti">$'+value+'</div>');
@@ -43,7 +43,7 @@ Humble( function () {
             slider.append(widget);
 
             config = {
-                max : total,
+                max : 1000,
                 value : value
             };
 
@@ -56,6 +56,27 @@ Humble( function () {
             this.node.delegate('.slider', 'slide', {this : this}, this._onSlide);
         },
 
+        update : function () {
+
+            var sliders = this._sliders;
+
+            var items   = this.model.getItems(),
+                node    = this.node;
+
+            _.each(sliders, function (slider, key) {
+
+                var value = items[key]['mycosti'];
+
+                slider = slider.find('.ui-slider');
+                value  = this._translate(value);
+
+                $(slider).slider('value', [value]);
+
+            }, this);
+
+            this.bind();
+        },
+
         _onSlide : function (e, ui) {
 
             var key     = $(this).data('key'),
@@ -63,6 +84,21 @@ Humble( function () {
 
             $(this).find('.mycosti').html('$'+ui.value);
             sliders.model.set(key, 'mycosti', ui.value);
+        },
+
+        /**
+         * Slider Value
+         *
+         * Slider goes from 0 to 100
+         */
+        _translate : function (value) {
+
+            // Income
+            var max = 50000;
+
+            value = (value / max) * 1000;
+
+            return value;
         }
     }
 
