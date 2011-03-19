b4d4abacccb7070d76af9718b146cb6fd3b6aceb
Carl Sutherland <carl@humblesoftware.com>
2011-03-19 12:44:55 -0400

    Added an app-scoped event manager.

--- js/Humble.js
+++ js/Humble.js
@@ -1,2 +1,12 @@
 // Declare Humble Namespace
 var Humble = function (callback) {callback.call()};
+// Global events, jQuery API.
+Humble.Event = {
+    hostage : $('<div></div>'),
+    trigger : function () {
+        this.hostage.trigger.apply(this.hostage, arguments);
+    },
+    bind : function () {
+        this.hostage.bind.apply(this.hostage, arguments);
+    }
+}
