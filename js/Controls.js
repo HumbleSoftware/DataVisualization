b9071613697ac8e8da6511c495aa444faccade24
Carl Sutherland <carl@humblesoftware.com>
2011-03-19 13:43:12 -0400

    Added total budget display to controls.

--- js/Controls.js
+++ js/Controls.js
@@ -5,7 +5,8 @@ Humble( function () {
 
     var T_INCOME_INPUT  = '<input id="humble-dvc-input" type="text"></input>',
         T_INCOME_LABEL  = '<label for="humble-dvc-input">Income</input>',
-        T_TOTAL_TAXES   = '<div class="humble-dvc-total-taxes"></div>';
+        T_TOTAL_TAXES   = '<div class="humble-dvc-total-taxes"></div>',
+        T_TOTAL_BUDGET  = '<div class="humble-dvc-total-budget"></div>';
 
     var Controls = function (node, model) {
 
@@ -21,10 +22,21 @@ Humble( function () {
 
             this._income    = $(T_INCOME_INPUT);
             this._total     = $(T_TOTAL_TAXES);
+            this._budget    = $(T_TOTAL_BUDGET);
 
             this.node.append(T_INCOME_LABEL);
             this.node.append(this._income);
             this.node.append(this._total);
+            this.node.append(this._budget);
+
+            this.bind();
+        },
+
+        bind : function () {
+            var that = this;
+            Humble.Event.bind('humble:dvc:modelUpdate', function (e) {
+                that.setBudget(that.model.getTotalSpending());
+            });
         },
 
         incomeChange : function (callback) {
@@ -38,6 +50,9 @@ Humble( function () {
 
         setTotal : function (total) {
             this._total.html('Total Taxes: $'+total);
+        },
+        setBudget : function (budget) {
+            this._budget.html('Federal Budget: $'+budget);
         }
 
     };
