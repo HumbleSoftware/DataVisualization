4903c7cc8f6e49e22bc03e718bdf4fadabd796f2
Carl Sutherland <carl@humblesoftware.com>
2011-03-20 12:45:59 -0400

    Implementing Dimension.

--- js/Application.js
+++ js/Application.js
@@ -3,17 +3,20 @@ function Application (applicationNode) {
     // DOM Templates
     var T_APPLICATION   = '<div class="humble-dvc-application"></div>',
         T_CONTROLS      = '<div class="humble-dvc-controls"></div>',
+        T_DIMENSION     = '<div class="humble-dvc-dimension"></div>',
         T_SLIDERS       = '<div class="humble-dvc-sliders"></div>',
         T_VISUAL        = '<div class="humble-dvc-visual"></div>';
 
     // DOM Nodes
     var rootNode        = $(T_APPLICATION),
         controlsNode    = $(T_CONTROLS),
+        dimensionNode   = $(T_DIMENSION),
         sliderNode      = $(T_SLIDERS),
         visualNode      = $(T_VISUAL);
 
     // Build that DOM
     rootNode.append(controlsNode);
+    rootNode.append(dimensionNode);
     rootNode.append(sliderNode);
     rootNode.append(visualNode);
     $(applicationNode).append(rootNode);
@@ -22,6 +25,7 @@ function Application (applicationNode) {
     var dataSource  = new Humble.DataSource(),
         model       = new Humble.Model(),
         controls    = new Humble.Controls(controlsNode, model),
+        dimension   = new Humble.Dimension(dimensionNode, model),
         sliders     = new Humble.Sliders(sliderNode, model);
         visual      = new Humble.Visual(visualNode, model);
 
