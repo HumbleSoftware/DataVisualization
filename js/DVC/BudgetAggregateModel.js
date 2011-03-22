fc1d28f04c06738cdb1ffa2c6bc2f1e43bd0ce2f
Carl Sutherland <carl@humblesoftware.com>
2011-03-22 12:44:26 -0400

    Subclass BudgetModel refactored out of BAM.

--- js/DVC/BudgetAggregateModel.js
+++ js/DVC/BudgetAggregateModel.js
@@ -15,37 +15,11 @@ Humble( function () {
 
     // Constructor
     var BudgetAggregateModel = function (options) {
-
-        this.url = options.url;
-        this.key = options.key;
-
-        Humble.Model.apply(this, arguments);
-        this.setters = {
-            'mycosti' : this._setMycosti
-        }
-        this.parsers = {
-            'amounti' : parseFloat,
-            'mycosti' : parseFloat
-        }
+        Humble.DVC.BudgetModel.apply(this, arguments);
     }
 
     // Methods
     BudgetAggregateModel.prototype = {
-
-        _setMycosti : function (item, attribute, value) {
-
-            var income  = this.getIncome(),
-                total   = this.getTotalTaxes(),
-                current = this.items[item][attribute];
-
-            if (total - current + value > income) {
-                value = income - total + current;
-            }
-
-            // Update amounti
-            var amounti = this._calculateAmountI(value);
-            this._set(item, 'amounti', amounti);
-        },
 
         set : function () {
             Humble.Model.prototype.set.apply(this, arguments);
@@ -61,63 +35,8 @@ Humble( function () {
             Humble.Event.trigger('humble:dvc:modelUpdate');
         },
 
-        setIncome : function (income) {
-
-            var config;
-
-            this.income = income;
-
-            config = {
-                data : { income : income }
-            };
-
-            this.dataSource.request(config);
-        },
-
-        getIncome : function () {
-            return this.income;
-        },
-
         getRatio : function () {
             return this.ratio;
-        },
-
-        getTotalTaxes : function () {
-            return this._getTotalNumeric('mycosti');
-        },
-
-        getTotalSpending : function () {
-            return this._getTotalNumeric('amounti');
-        },
-
-        getMaxSpending : function () {
-
-            var values = this.getItems(),
-                max    = 0,
-                field  = 'amounti';
-
-            _.each(values, function (value, key) {
-                max = Math.max(max, value[field]);
-            }, this);
-
-            return max;
-        },
-
-        requestData : function () {
-
-            var data = {};
-
-            // Data
-            data[YEAR]      = 2010;
-            data[TYPE]      = 0;
-            data[SORT]      = 0;
-            data[INCOME]    = 50000;
-            data[FILING]    = 0;
-            data[GROUP]     = 'function';
-            data[CHANGE]    = 0;
-            data[EXTRA]     = 0;
-
-            return data;
         },
 
         _getRatio : function () {
@@ -127,14 +46,10 @@ Humble( function () {
 
             return (totalTaxes) ?
                 (totalSpending / totalTaxes) : false;
-        },
-
-        _calculateAmountI : function (mycosti) {
-            return this.getRatio() * mycosti;
         }
     };
 
-    Humble.Class.extend(BudgetAggregateModel, Humble.Model);
+    Humble.Class.extend(BudgetAggregateModel, Humble.DVC.BudgetModel);
 
     // Namespace Hook
     Humble.DVC.BudgetAggregateModel = BudgetAggregateModel;
---
+++ js/DVC/BudgetModel.js
@@ -0,0 +1,125 @@
+/**
+ * Class Humble Model
+ */
+Humble( function () {
+
+    // Class Constants
+    var CHANGE      = 'showChange',
+        EXTRA       = 'showExtra',
+        FILING      = 'filing',
+        GROUP       = 'group',
+        INCOME      = 'income',
+        SORT        = 'sortdir',
+        TYPE        = 'type',
+        YEAR        = 'year';
+
+    // Constructor
+    var BudgetModel = function (options) {
+
+        if (options) {
+            this.url = options.url;
+            this.key = options.key;
+        }
+
+        Humble.Model.apply(this, arguments);
+        this.setters = {
+            'mycosti' : this._setMycosti
+        }
+        this.parsers = {
+            'amounti' : parseFloat,
+            'mycosti' : parseFloat
+        }
+    }
+
+    // Methods
+    BudgetModel.prototype = {
+
+        _setMycosti : function (item, attribute, value) {
+
+            var income  = this.getIncome(),
+                total   = this.getTotalTaxes(),
+                current = this.items[item][attribute];
+
+            if (total - current + value > income) {
+                value = income - total + current;
+            }
+
+            // Update amounti
+            var amounti = this._calculateAmountI(value);
+            this._set(item, 'amounti', amounti);
+        },
+
+        setIncome : function (income) {
+
+            var config;
+
+            this.income = income;
+
+            config = {
+                data : { income : income }
+            };
+
+            this.dataSource.request(config);
+        },
+
+        getIncome : function () {
+            return this.income;
+        },
+
+        getTotalTaxes : function () {
+            return this._getTotalNumeric('mycosti');
+        },
+
+        getTotalSpending : function () {
+            return this._getTotalNumeric('amounti');
+        },
+
+        getMaxSpending : function () {
+
+            var values = this.getItems(),
+                max    = 0,
+                field  = 'amounti';
+
+            _.each(values, function (value, key) {
+                max = Math.max(max, value[field]);
+            }, this);
+
+            return max;
+        },
+
+        requestData : function () {
+
+            var data = {};
+
+            // Data
+            data[YEAR]      = 2010;
+            data[TYPE]      = 0;
+            data[SORT]      = 0;
+            data[INCOME]    = 50000;
+            data[FILING]    = 0;
+            data[GROUP]     = 'function';
+            data[CHANGE]    = 0;
+            data[EXTRA]     = 0;
+
+            return data;
+        },
+
+        _getRatio : function () {
+
+            var totalSpending   = this.getTotalSpending(),
+                totalTaxes      = this.getTotalTaxes();
+
+            return (totalTaxes) ?
+                (totalSpending / totalTaxes) : false;
+        },
+
+        _calculateAmountI : function (mycosti) {
+            return this.getRatio() * mycosti;
+        }
+    };
+
+    Humble.Class.extend(BudgetModel, Humble.Model);
+
+    // Namespace Hook
+    Humble.DVC.BudgetModel = BudgetModel;
+});
