3e2948fd232165b26bcf0eb1c4051bd8a65acef9
Carl Sutherland <carl@humblesoftware.com>
2011-03-22 21:42:24 -0400

    Adding budget account model.

---
+++ js/DVC/BudgetAccountModel.js
@@ -0,0 +1,39 @@
+/**
+ * Class Humble Model
+ */
+Humble( function () {
+
+    // Class Constants
+    var CHANGE      = 'showChange',
+        EXTRA       = 'showExtra',
+        FILING      = 'filing',
+        GROUP       = 'group',
+        INCOME      = 'income',
+        SORT        = 'sortdir',
+        TYPE        = 'type',
+        YEAR        = 'year';
+
+    // Constructor
+    var BudgetAccountModel = function (options) {
+        Humble.DVC.BudgetModel.apply(this, arguments);
+    }
+
+    // Methods
+    BudgetAccountModel.prototype = {
+
+        set : function () {
+            Humble.Model.prototype.set.apply(this, arguments);
+            Humble.Event.trigger('humble:dvc:accountModelUpdate');
+        },
+
+        setXML : function (xml) {
+            Humble.Model.prototype.setXML.apply(this, arguments);
+            Humble.Event.trigger('humble:dvc:accountModelUpdate');
+        },
+    };
+
+    Humble.Class.extend(BudgetAccountModel, Humble.DVC.BudgetModel);
+
+    // Namespace Hook
+    Humble.DVC.BudgetAccountModel = BudgetAccountModel;
+});
