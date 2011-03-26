6e0d2e9b400811e8a9bf42f34ee1cc576c2ead62
Jordan Santell <jsantell@gmail.com>
2011-03-25 21:04:16 -0700

    Easier Config color manipulation, makes half of the dimensions opaque (need a desaturation->hex function)

--- js/Config.js
+++ js/Config.js
@@ -18,6 +18,19 @@ Humble.Config.DVZ.budgetAccount = {
         mycosti : {}
     }
 };
+
+var Color  = new Array(
+    '#ff0049', //strawberry
+    '#fd7100', //red-orange
+    '#f9aa00', //orange
+    '#f9e502', //yellow
+    '#70fb00', //green
+    '#00c2ff', //light blue
+    '#0f91ff', //blue
+    '#b537b6', //purple
+    '#e92192'  //fuschia
+);
+
 Humble.Config.DVZ.budget = {
     name : 'getBudget',
     fields : {
@@ -34,27 +47,27 @@ Humble.Config.DVZ.budget = {
     dimensions : {
         _2771 : {
             name  : "National Defense",
-            color : '#ff0049'
+            color : Color[0]
         },
         _2772 : {
             name  : "International Affairs",
-            color : '#f9aa00'
+            color : Color[1]
         },
         _2773 : {
             name  : "General Science, Space, and Technology",
-            color : '#f9e502'
+            color : Color[2]
         },
         _2774 : {
             name  : "Energy",
-            color : '#c2d618'
+            color : Color[3]
         },
         _2775 : {
             name  : "Natural Resources and Environment",
-            color : '#70fb00'
+            color : Color[4]
         },
         _2776 : {
             name  : "Agriculture",
-            color : '#00c2ff'
+            color : Color[5]
         },
 //        _2777 : {
 //            name  : "Commerce and Housing Credit",
@@ -62,51 +75,51 @@ Humble.Config.DVZ.budget = {
 //        },
         _2778 : {
             name  : "Transportation",
-            color : '#0f91ff'
+            color : Color[6]
         },
         _2779 : {
             name  : "Community and Regional Development",
-            color : '#b537b6'
+            color : Color[7]
         },
         _2780 : {
             name  : "Education, Training, Employment, and Social Services",
-            color : '#e92192'
+            color : Color[8]
         },
         _2781 : {
             name  : "Health",
-            color : '#ff0049'
+            color : Color[0]
         },
         _2782 : {
             name  : "Medicare",
-            color : '#f9aa00'
+            color : Color[1]
         },
         _2783 : {
             name  : "Income Security",
-            color : '#f9e502'
+            color : Color[2]
         },
         _2784 : {
             name  : "Social Security",
-            color : '#c2d618'
+            color : Color[3]
         },
         _2785 : {
             name  : "Veterans Benefits and Services",
-            color : '#70fb00'
+            color : Color[4]
         },
         _2786 : {
             name  : "Administration of Justice",
-            color : '#00c2ff'
+            color : Color[5]
         },
         _2787 : {
             name  : "General Government",
-            color : '#0f91ff'
+            color : Color[6]
         },
         _2788 : {
             name  : "Net Interest",
-            color : '#b537b6'
+            color : Color[7]
         },
         _2789 : {
             name  : "Allowances",
-            color : '#e92192'
+            color : Color[8]
         }
 //        _2790 : {
 //            name  : "Undistributed Offsetting Receipts",
