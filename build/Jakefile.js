dc32159a9ac7218e7e521518e1bd47abce980304
Carl Sutherland <carl@humblesoftware.com>
2011-06-11 12:03:44 -0400

    Finished working build script.

--- build/Jakefile.js
+++ build/Jakefile.js
@@ -1,33 +1,58 @@
 var fs = require('fs');
+var sys = require('sys');
 var path = require('path');
 var config = require('./config.js');
 var exec = require('child_process').exec;
-var path = '../';
 
 desc('This is the default task');
-task('default', [], function (params) {
-
+task('default', function (params) {
     console.log('Starting default task...');
-    for (group in config.groups) {
+});
 
-        console.log();
-        console.log('Starting group "'+group+'":');
+task('clean', function (params) {
+    console.log('Executing task clean...');
+    try {
+        fs.unlinkSync('dvc.js');
+    } catch (e) {}
 
-        config.groups[group].forEach(function (file, index) {
-            console.log(file);
-            build();
-        });
-
-        console.log('Group complete.');
-    }
-
-    console.log();
     console.log('Build complete.');
+    console.log('');
+});
+
+task({'build' : [
+    'dvc.js',
+    'asynctest'
+]}, function () {
+    console.log('Build complete.');
+});
+
+file({'dvc.js': config.groups.dvc}, function () {
+    console.log('building test.js');
+
+    var files   = this.prereqs,
+        outFile = this.name;
+
+    concatSync(files, outFile);
+    minify(outFile);
+
 }, true);
 
-function build (files) {
-    exec('ls', function (error, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+task('asynctest', function () {
+    setTimeout(complete, 1000);
+}, true);
+
+function concatSync(files, outFile) {
+    var output = '';
+    files.forEach(function (file) {
+        output += fs.readFileSync(file);
+    });
+    fs.writeFileSync(outFile, output);
+    return outFile;
+}
+
+function minify (inFile, outFile) {
+    var cmd = 'java -jar ./tools/yuicompressor-2.4.5.jar -o '+(outFile || inFile)+' '+inFile;
+    exec(cmd, function (error, stdout, stderr) {
+        complete();
     });
 }
--- build/config.js
+++ build/config.js
@@ -1,7 +1,17 @@
+var PREFIX = '../js/'
 exports.groups = {}
 exports.groups.humble = [
-    'js/*'
+    '../js/Application.js'
 ]
 exports.groups.dvc = [
-    'js/DVC/*'
+    PREFIX + 'Humble.js',
+    PREFIX + 'Application.js',
+    PREFIX + 'Config.js',
+    PREFIX + 'Controls.js',
+    PREFIX + 'DataSource.js',
+    PREFIX + 'Dimension.js',
+    PREFIX + 'Formatter.js',
+    PREFIX + 'Model.js',
+    PREFIX + 'Sliders.js',
+    PREFIX + 'Visual.js'
 ]
