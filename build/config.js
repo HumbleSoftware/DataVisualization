c6fdc7bbf343d40ce6f5919ffbcff99b9df2f0e9
Carl Sutherland <carl@humblesoftware.com>
2011-06-11 13:46:41 -0400

    Added stylesheet to config.

--- build/config.js
+++ build/config.js
@@ -1,17 +1,28 @@
-var PREFIX = '../js/'
-exports.groups = {}
-exports.groups.humble = [
-    '../js/Application.js'
-]
+/**
+ * DVC Build Config
+ */
+
+var PREFIX_JS   = '../js/',
+    PREFIX_CSS  = '../css/'
+
+// Minifiy Groups
+exports.groups = {};
+
+// DVC StyleSheets
+exports.groups.stylesheet = [
+    PREFIX_CSS + 'stylesheet.css'
+];
+
+// DVC JavaScript
 exports.groups.dvc = [
-    PREFIX + 'Humble.js',
-    PREFIX + 'Application.js',
-    PREFIX + 'Config.js',
-    PREFIX + 'Controls.js',
-    PREFIX + 'DataSource.js',
-    PREFIX + 'Dimension.js',
-    PREFIX + 'Formatter.js',
-    PREFIX + 'Model.js',
-    PREFIX + 'Sliders.js',
-    PREFIX + 'Visual.js'
-]
+    PREFIX_JS + 'Humble.js',
+    PREFIX_JS + 'Application.js',
+    PREFIX_JS + 'Config.js',
+    PREFIX_JS + 'Controls.js',
+    PREFIX_JS + 'DataSource.js',
+    PREFIX_JS + 'Dimension.js',
+    PREFIX_JS + 'Formatter.js',
+    PREFIX_JS + 'Model.js',
+    PREFIX_JS + 'Sliders.js',
+    PREFIX_JS + 'Visual.js'
+];
