162174b79db9eab0393b0372c7c6e9cce12e1a51
Carl Sutherland <carl@humblesoftware.com>
2011-06-11 13:16:31 -0400

    Beginning build file structure.

--- build/Jakefile.js
+++ build/Jakefile.js
@@ -4,42 +4,55 @@ var path = require('path');
 var config = require('./config.js');
 var exec = require('child_process').exec;
 
+var target = 'target';
+
+// Default Task
 desc('This is the default task');
 task('default', function (params) {
     console.log('Starting default task...');
 });
 
+// Clean
 task('clean', function (params) {
     console.log('Executing task clean...');
     try {
-        fs.unlinkSync('dvc.js');
+        console.log('  Removing "'+target+'".');
+        exec('rm -rf '+target, function (error, stdout, stderr) {
+            complete();
+            console.log('Build complete.');
+            console.log('');
+        });
     } catch (e) {}
+}, true);
 
-    console.log('Build complete.');
-    console.log('');
-});
-
+// Build
 task({'build' : [
-    'dvc.js',
-    'asynctest'
+    'pre-build',
+    'target/js/dvc.js'
 ]}, function () {
     console.log('Build complete.');
 });
 
-file({'dvc.js': config.groups.dvc}, function () {
-    console.log('building test.js');
+// Pre-build
+task('pre-build', function () {
+    fs.mkdirSync(target, '755');
+    fs.mkdirSync(target+'/js', '755');
+    fs.mkdirSync(target+'/css', '755');
+});
 
+// dvc.js
+file({'target/js/dvc.js': config.groups.dvc}, function () {
     var files   = this.prereqs,
         outFile = this.name;
-
+    console.log(outFile);
     concatSync(files, outFile);
     minify(outFile);
-
 }, true);
 
-task('asynctest', function () {
-    setTimeout(complete, 1000);
-}, true);
+
+/**
+ * Helper Methods
+ */
 
 function concatSync(files, outFile) {
     var output = '';
